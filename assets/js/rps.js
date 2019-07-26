f67c3f6dd4fc8fba314576d3db8081f1aee8a736
ingan <isabelngan27@gmail.com>
2019-07-25 23:08:16 -0700

    got beginning chat and user display update

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -124,355 +124,82 @@ $(document).ready(function () {
         }
     });
 
-    // listen to the players node of the database to see if anything changes
+    // any database changes
     database.ref("/players/").on("value", function(snapshot) {
 
-        // Check to see if player 1 exists in our database - if it does, set variables and display
-        // player 1's name and data. If it doesn't exist, display waiting for player 1 
+        // Check to see if player 1 exists in our database 
     	if (snapshot.child("p1").exists()) {
-
     		// player 1 variables
     		p1 = snapshot.val().p1;
     		p1name = p1.name;
 
-    // 		// display player 1's name and score data 
-    // 		$("#p1display").text(`${p1name} has ${p1.win} wins ${p1.loss} losses and ${p1.tie} ties`);
-    //     } 
+    		// display player 1 name and score data 
+            $("#player1name").text(p1name);
+            $("#player1win").text(p1.win);
+            $("#player1lose").text(p1.lose);
+        } else {
+    		console.log("Player 1 does NOT exist in the database");
+    		p1 = null;
+    		p1name = "";
+    		// reset p1
+            $("#player1name").text("Waiting for Player 1...");
+            $("#player1win").text("0");
+            $("#player1lose").text("0");
+    		database.ref("/outcome/").remove();
+    		$("#results").text("Waiting for all players to join.");
+        }
 
-    //     // what happens if player 1 doesn't exist?
-    //     else {
-    // 		console.log("Player 1 does NOT exist in the database");
-    // 		p1 = null;
-    // 		p1name = "";
+         // Check to see if player 2 exists in our database 
+        if (snapshot.child("p2").exists()) {
+    		// player 2 variables
+    		p2 = snapshot.val().p2;
+    		p2name = p2.name;
 
-    // 		// Display "empty" state for p1 
-    // 		$("#p1name").text("Waiting for Player 1...");
-    // 		database.ref("/outcome/").remove();
-    // 		$("#outcome").html("Rock! Paper! Scissors! Shoot!");
-    // 		$("#p2data").html("Wins: 0 Losses: 0 Ties: 0");
-    //     }
+    		// display player 2 name and score data 
+            $("#player2name").text(p2name);
+            $("#player2win").text(p2.win);
+            $("#player2lose").text(p2.lose);
+        }  else {
+    		console.log("Player 2 does NOT exist in the database");
+    		p2 = null;
+    		p2name = "";
 
-    //     if (snapshot.child("p2").exists()) {
-    // 		console.log("Player 2 exists in the database");
+    		// Reset p2
+            $("#player2name").text("Waiting for Player 2...");
+            $("#player2win").text("0");
+            $("#player2lose").text("0");
+            database.ref("/outcome/").remove();
+        }
 
-    // 		// set local variables for player 1
-    // 		p2 = snapshot.val().p2;
-    // 		p2name = p2.name;
+        // If both players are now present, it's p1's turn
+    	if (p1 && p2) {
+            $("#results").text("Game Time");
+            console.log("Both players are now present")
 
-    // 		// display player 1's name and score data 
-    // 		$("#p2display").text(`${p2name} has ${p2.win} wins ${p2.loss} losses and ${p2.tie} ties`);
-    //     } 
+            // green border around player turn (rn is player 1)
+            $("#player1").addClass("yourTurn");
+    		// Update the center display
+    		$("#results").text("Waiting for " + p1name + " to choose...");
+        }
+        	// reset chat, turn, and results display
+    	if (!p1 && !p2) {
+    		database.ref("/chat/").remove();
+    		database.ref("/turn/").remove();
+    		database.ref("/outcome/").remove();
+    		$("#chatdisplay").empty();
+    		$("#p1display").removeClass("yourTurn");
+    		$("#p2display").removeClass("yourTurn");
+    		$("#results").text("Waiting for all players to join.");
+    	}
+    });
+    // any disconections
+    database.ref("/players/").on("child_removed", function(snapshot) {
+    	var msg = snapshot.val().name + " has disconnected!";
+    	// use a unique key for the disconnection chat entry
+    	var chatKey = database.ref().child("/chat/").push().key;
+    	// save the disconnection chat entry to the database with the key
+    	database.ref("/chat/" + chatKey).set(msg);
+    });
 
-    //     // what happens if player 1 doesn't exist?
-    //     else {
-    // 		console.log("Player 2 does NOT exist in the database");
-    // 		p2 = null;
-    // 		p2name = "";
-
-    // 		// Display "empty" state for p1 
-    // 		$("#p2name").text("Waiting for Player 2...");
-    // 		database.ref("/outcome/").remove();
-    // 		$("#outcome").html("Rock! Paper! Scissors! Shoot!");
-    // 		$("#scoreboard").html("Waiting for Players to join");
-    // 		$("#p2data").html("Wins: 0 Losses: 0 Ties: 0");
-    //     }
-
-    //     // If both players are now present, it's p1's turn
-    // 	if (p1 && p2) {
-    //         console.log("Both players are now present")
-    // 		// Update the display with a green border around player 1
-    // 		$("#p1display").addClass("yourTurn");
-
-    // 		// Update the center display
-    // 		$("#outcome").html("Waiting on " + p1name + " to choose...");
-    //     }
-
-    //     	// If both players leave the game, empty the chat session
-    // 	if (!p1 && !p2) {
-    // 		database.ref("/chat/").remove();
-    // 		database.ref("/turn/").remove();
-    // 		database.ref("/outcome/").remove();
-    // 		$("#chatdisplay").empty();
-    // 		$("#p1display").removeClass("yourTurn");
-    // 		$("#p2display").removeClass("yourTurn");
-    // 		$("#outcome").html("Rock! Paper! Scissors! Shoot!");
-    // 	}
-
-    // });
-    // // The database is listening for users to disconnect via "child_removed"
-    // // disconnection happens by closing the window or refreshing the page
-    // database.ref("/players/").on("child_removed", function(snapshot) {
-    // 	var msg = snapshot.val().name + " has disconnected!";
-
-    // 	// use a unique key for the disconnection chat entry
-    // 	var chatKey = database.ref().child("/chat/").push().key;
-
-    // 	// save the disconnection chat entry to the database with the key
-    // 	database.ref("/chat/" + chatKey).set(msg);
-    // });
-
-    // // The database is listening for any new chat messages via "child_added"
-    // // inside of the chat keyvalue pair 
-    // database.ref("/chat/").on("child_added", function(snapshot) {
-    // 	// get the new message from the database
-    // 	var chatMsg = snapshot.val();
-    // 	// make a chat variable to append new chat lines into divs
-    // 	var chatEntry = $("<div>").html(chatMsg);
-
-    // 	// if YOU sent the chat message, the name appears in red
-    // 	if (chatMsg.startsWith(yourPlayerName)) {
-    // 		chatEntry.addClass("p1color");
-    // 	// if you DID NOT send the chat message, the name appears in blue
-    // 	} else {
-    // 		chatEntry.addClass("p2color");
-    // 	}
-
-    // 	// append the div stored in chatEntry with its new styling to the DOM
-    // 	$("#chatdisplay").append(chatEntry);
-    // 	// use ScrollHeight to keep the chat going
-    // 	$("#chatdisplay").scrollTop($("#chatdisplay")[0].scrollHeight);
-    // });
-
-    // // The database is listening for the "turn" variable to change
-    // // it only changes between 1 and 2 depending on whether it is 
-    // // player 1's turn or player 2's turn
-
-    // database.ref("/turn/").on("value", function(snapshot) {
-    // 	// Check if it's p1's turn
-    // 	if (snapshot.val() === 1) {
-    // 		console.log("turn 1");
-    // 		turn = 1;
-    // 		// make sure that both players have joined the game
-    // 		// then make the p1 display pane green if it is player 1's
-    // 		// turn and makes sure players 2 panel is not green
-    // 		if (p1 && p2) {
-    // 			$("#p1display").addClass("yourTurn");
-    // 			$("#p2display").removeClass("yourTurn");
-    // 			// show when p1 still has to pick
-    // 			$("#outcome").html("Waiting on " + p1name + " to choose...");
-    // 		}
-    // 		//otherwise, do the opposite for player 2 and make their
-    // 		// panel green and p2's panel not green 
-    // 	} else if (snapshot.val() === 2) {
-    // 		console.log("TURN 2");
-    // 		turn = 2;
-    // 		if (p1 && p2) {
-    // 			$("#p1display").removeClass("yourTurn");
-    // 			$("#p2display").addClass("yourTurn");
-
-    // 			// show when p2 still has to pick 
-    // 			$("#outcome").html("Waiting on " + p2name + " to choose...");
-    // 		}
-    // 	}
-    // });
-
-    // // The database is listening for the send button to be clicked on chat - we
-    // // have to use preventDefault here because it's a form submit button 
-    // // and we don't want the page to be refreshed when it is clicked
-    // $("#chat-send").on("click", function(event) {
-    // 	event.preventDefault();
-
-    // 	// First, make sure that the player exists and the message textbox has text in it
-    // 	if ( (yourPlayerName !== "") && ($("#chat-text").val().trim() !== "") ) {
-    // 		// Grab the message from the input box and subsequently reset the input box
-    // 		var msg = yourPlayerName + ": " + $("#chat-text").val().trim();
-    // 		$("#chat-text").val("");
-
-    // 		// Get a key for the new chat entry
-    // 		var chatKey = database.ref().child("/chat/").push().key;
-
-    // 		// Save the new chat entry
-    // 		database.ref("/chat/" + chatKey).set(msg);
-    // 	}
-    // });
-
-    // // The database has to listen for player 1 to press one of the
-    // // three possible buttons, but only when turn = 1. Therefore 
-    // // the buttons are inactive for player 1 when it is player 2's 
-    // // turn and vice-versa. The inverse applies below for player 2 
-
-    // // There are rock, paper, and scissor button handlers for p1 
-    // // and p2 respectively. I did think that you could intelligently
-    // // figure out which player clicked the button by their name or even
-    // // using the css class applied to their chat color, but it was a stretch
-    // // goal for this and I ran out of time
-    // $("#rock").on("click", function() {
-    // 	// buttons are only active if there are two players and turn is 1
-    // 	if (p1 && p2 && (yourPlayerName === p1.name) && (turn === 1) ) {
-    // 		// grab the text value from the button using this and
-    // 		// assign it to the choice variable
-    // 		var choice = $(this).text().trim();
-    // 		console.log("player selected "+  $(this).text());
-    // 		p1Choice = choice;
-    // 		// write the choice variable to the database for p1
-    // 		database.ref().child("/players/p1/choice").set(choice);
-
-    // 		// Set the turn value to 2, as it is now p2's turn
-    // 		turn = 2;
-    // 		database.ref().child("/turn").set(2);
-    // 	}
-    // });
-
-    // $("#paper").on("click", function() {
-    // 	// Make selections only when both players are in the game
-    // 	if (p1 && p2 && (yourPlayerName === p1.name) && (turn === 1) ) {
-    // 		// Record p1's choice
-    // 		var choice = $(this).text().trim();
-    // 		console.log("player selected "+  $(this).text());
-    // 		// Record the player choice into the database
-    // 		p1Choice = choice;
-    // 		database.ref().child("/players/p1/choice").set(choice);
-
-    // 		// Set the turn value to 2, as it is now p2's turn
-    // 		turn = 2;
-    // 		database.ref().child("/turn").set(2);
-    // 	}
-    // });
-
-    // $("#scissors").on("click", function() {
-    // 	// Make selections only when both players are in the game
-    // 	if (p1 && p2 && (yourPlayerName === p1.name) && (turn === 1) ) {
-    // 		// Record p1's choice
-    // 		var choice = $(this).text().trim();
-    // 		console.log("player selected "+  $(this).text());
-    // 		// Record the player choice into the database
-    // 		p1Choice = choice;
-    // 		database.ref().child("/players/p1/choice").set(choice);
-
-    // 		// Set the turn value to 2, as it is now p2's turn
-    // 		turn = 2;
-    // 		database.ref().child("/turn").set(2);
-    // 	}
-    // });
-
-    // // listen for p2 to click the options when it is their turn
-    // // see the comment for p1's #rock click handler function
-    // // for more details, this is just copied and pasted and variables 
-    // // swapped from p1 to p2 for rock, paper, and scissors and turn
-    // $("#rock").on("click", function() {
-    // 	event.preventDefault();
-
-    // 	// Make selections only when both players are in the game
-    // 	if (p1 && p2 && (yourPlayerName === p2.name) && (turn === 2) ) {
-    // 		// Record p2's choice
-    // 		var choice = $(this).text().trim();
-
-    // 		// Record the player choice into the database
-    // 		p2Choice = choice;
-    // 		database.ref().child("/players/p2/choice").set(choice);
-
-    // 		// Compare p1 and player 2 choices and record the outcome
-    // 		compareChoices();
-    // 	}
-    // });
-
-    // $("#paper").on("click", function() {
-    // 	event.preventDefault();
-
-    // 	// Make selections only when both players are in the game
-    // 	if (p1 && p2 && (yourPlayerName === p2.name) && (turn === 2) ) {
-    // 		// Record p2's choice
-    // 		var choice = $(this).text().trim();
-
-    // 		// Record the player choice into the database
-    // 		p2Choice = choice;
-    // 		database.ref().child("/players/p2/choice").set(choice);
-
-    // 		// Compare p1 and player 2 choices and record the outcome
-    // 		compareChoices();
-    // 	}
-    // });
-
-    // $("#scissors").on("click", function() {
-    // 	event.preventDefault();
-
-    // 	// Make selections only when both players are in the game
-    // 	if (p1 && p2 && (yourPlayerName === p2.name) && (turn === 2) ) {
-    // 		// Record p2's choice
-    // 		var choice = $(this).text().trim();
-
-    // 		// Record the player choice into the database
-    // 		p2Choice = choice;
-    // 		database.ref().child("/players/p2/choice").set(choice);
-
-    // 		// Compare p1 and player 2 choices and record the outcome
-    // 		compareChoices();
-    // 	}
-    // });
-
-    // //classic RPS game logic to pick a winner 
-    // function compareChoices() {
-    // 	if (p1.choice === "Rock") {
-    // 		if (p2.choice === "Rock") {
-    // 			// Tie
-    // 			console.log("tie");
-    // 			database.ref().child("/outcome/").set("Tie game!");
-    // 			database.ref().child("/players/p1/tie").set(p1.tie + 1);
-    // 			database.ref().child("/players/p2/tie").set(p2.tie + 1);
-    // 		} else if (p2.choice === "Paper") {
-    // 			// p2 wins
-    // 			console.log("paper wins");
-    // 			database.ref().child("/outcome/").set("Paper wins!");
-    // 			database.ref().child("/players/p1/loss").set(p1.loss + 1);
-    // 			database.ref().child("/players/p2/win").set(p2.win + 1);
-    // 		} else { // scissors
-    // 			// p1 wins
-    // 			console.log("rock wins");
-    // 			database.ref().child("/outcome/").set("Rock wins!");
-    // 			database.ref().child("/players/p1/win").set(p1.win + 1);
-    // 			database.ref().child("/players/p2/loss").set(p2.loss + 1);
-    // 		}
-    // 	} else if (p1.choice === "Paper") {
-    // 		if (p2.choice === "Rock") {
-    // 			// p1 wins
-    // 			console.log("paper wins");
-    // 			database.ref().child("/outcome/").set("Paper wins!");
-    // 			database.ref().child("/players/p1/win").set(p1.win + 1);
-    // 			database.ref().child("/players/p2/loss").set(p2.loss + 1);
-    // 		} else if (p2.choice === "Paper") {
-    // 			// Tie
-    // 			console.log("tie");
-    // 			database.ref().child("/outcome/").set("Tie game!");
-    // 			database.ref().child("/players/p1/tie").set(p1.tie + 1);
-    // 			database.ref().child("/players/p2/tie").set(p2.tie + 1);
-    // 		} else { // Scissors
-    // 			// p2 wins
-    // 			console.log("scissors win");
-    // 			database.ref().child("/outcome/").set("Scissors win!");
-    // 			database.ref().child("/players/p1/loss").set(p1.loss + 1);
-    // 			database.ref().child("/players/p2/win").set(p2.win + 1);
-    // 		}
-    // 	} else if (p1.choice === "Scissors") {
-    // 		if (p2.choice === "Rock") {
-    // 			// p2 wins
-    // 			console.log("rock wins");
-
-    // 			database.ref().child("/outcome/").set("Rock wins!");
-    // 			database.ref().child("/players/p1/loss").set(p1.loss + 1);
-    // 			database.ref().child("/players/p2/win").set(p2.win + 1);
-    // 		} else if (p2.choice === "Paper") {
-    // 			// p1 wins
-    // 			console.log("scissors win");
-
-    // 			database.ref().child("/outcome/").set("Scissors win!");
-    // 			database.ref().child("/players/p1/win").set(p1.win + 1);
-    // 			database.ref().child("/players/p2/loss").set(p2.loss + 1);
-    // 		} else {
-    // 			// Tie
-    // 			console.log("tie");
-
-    // 			database.ref().child("/outcome/").set("Tie game!");
-    // 			database.ref().child("/players/p1/tie").set(p1.tie + 1);
-    // 			database.ref().child("/players/p2/tie").set(p2.tie + 1);
-    // 		}
-
-    // 	}
-
-    // 	// reset back to player 1's turn
-    // 	// this also helps make sure that for every "new game"
-    // 	// turn is set to 1 as a variable in this code
-    // 	// AND as an element in the database
-    // 	turn = 1;
-    // 	database.ref().child("/turn").set(1);
-    // }
+    
 });
